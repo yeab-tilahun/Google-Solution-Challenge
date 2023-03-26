6f430e44ad420c219f7d1ebc77de5a433d0c6e12
Beteab Admassu <beteab.admassu.tadele@gmail.com>
2023-03-26 14:06:36 +0300

    updated

--- React/src/component/Response.js
+++ React/src/component/Response.js
@@ -1,32 +1,6 @@
 import React, { useEffect,useCallback, useState } from "react";
 import Container from "./Container";
 
-// export default function Response(props) {
-
-
-//   const postData = useCallback(async () => {
-//     const data = { question: props.text };
-  
-//     const response = await fetch("https://0cbc-149-34-244-156.eu.ngrok.io/question", {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify(data)
-//     });
-  
-//     const result = await response.json();
-//     console.log(result);
-//   }, []);
-
-//    postData();
-
-
-
-
-//   // return  <Container key={Math.random()} text = {response}/>
-// }
-
 
 export default function Response({ text }) {
   const [response, setResponse] = useState(null);
